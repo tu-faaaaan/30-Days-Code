b8f1df32ec42a7b4a30cbdb2ff3b432220bfd304
Mohd Affan Siddiqui <footballeraffan7@gmail.com>
2023-02-02 12:05:16 +0530

    Day 1 code added

---
+++ Day1.cpp
@@ -0,0 +1,69 @@
+// Newton School
+// Question of the Day - Mall and Coupons
+
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+#define ll long long
+#define all(x) x.begin(), x.end()
+
+void solve()
+{
+    ll n, k, x;
+    cin >> n >> k >> x;
+
+    vector<ll> a(n);
+    for (auto &i : a)
+    {
+        cin >> i;
+    }
+
+    sort(a.begin(), a.end());
+    reverse(a.begin(), a.end());
+
+    ll ans = 0;
+    ll curr = 0;
+
+    for (auto &i : a)
+    {
+        ll req = i / x;
+        ll rem = i % x;
+
+        if (k >= req)
+        {
+            i = rem;
+            k -= req;
+        }
+        else
+        {
+            if (k != 0)
+            {
+                i -= k * x;
+                k = 0;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    sort(all(a));
+    reverse(all(a));
+
+    ll sum = 0;
+    for (int i = k; i < n; i++)
+    {
+        sum += a[i];
+    }
+
+    cout << sum << endl;
+}
+
+int main()
+{
+    // Your code here
+
+    solve();
+
+    return 0;
+}
