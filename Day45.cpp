5710aff6b2a254677a1af79780f2a0c0a533c390
Mohd Affan Siddiqui <footballeraffan7@gmail.com>
2023-03-19 23:41:29 +0530

    Added Day 45 Code

---
+++ Day45.cpp
@@ -0,0 +1,14 @@
+// Newton School
+// Question of the Day - Rohit and Balls
+// 02/03/2023
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, a, b;
+    cin >> n >> a >> b;
+    cout << (n - a + b);
+    return 0;
+}
